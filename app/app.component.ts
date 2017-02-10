7e830597bee84606d2911f1cecd2d52c17069dbe
aramg <aramg@DESKTOP-EEJF7JS>
2017-02-09 18:32:43 -0800

    Add project files.

---
+++ app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MDL } from './MaterialDesignLiteUpgradeElement';
+
+declare var __moduleName: string;
+
+@Component({
+    moduleId: __moduleName,
+    selector: 'my-app',
+    templateUrl: './app.component.html',
+    //directives: [MDL]
+})
+export class AppComponent {
+}
